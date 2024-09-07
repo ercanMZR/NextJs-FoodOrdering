66f5765abebc7774804d57b7fa78fb89df3d533a
Ercan MUZIR <ercanmuzir@gmail.com>
2024-09-07 16:08:05 +0300

    Menu wrapper, item component created

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Campaigns from '../components/ui/Campaigns';
 import Carousel from '../components/Carousel';
+import MenuWrapper from '../components/product/MenuWrapper';
 
 
 const Index = () => {
   return (
-    <div className="relative">
+    <div className="">
       <div>
       <Carousel />
       <Campaigns />
+      <MenuWrapper/>
       </div>
      
     </div>
