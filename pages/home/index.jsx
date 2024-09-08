fc358e1b737c3f55131d674fa6993dccaeb15da1
Ercan MUZIR <ercanmuzir@gmail.com>
2024-09-08 16:17:44 +0300

    created About component

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Campaigns from '../components/ui/Campaigns';
 import Carousel from '../components/Carousel';
 import MenuWrapper from '../components/product/MenuWrapper';
+import About from '../components/ui/About';
 
 
 const Index = () => {
@@ -11,6 +12,7 @@ const Index = () => {
       <Carousel />
       <Campaigns />
       <MenuWrapper/>
+      <About/>
       </div>
      
     </div>
