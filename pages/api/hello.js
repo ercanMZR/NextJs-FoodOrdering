18833b9fa5257e6a901cb68436466b1b36eaea2f
Ercan MUZIR <ercanmuzir@gmail.com>
2024-09-27 21:14:50 +0300

    DbConnection added and User's post and get endpoints

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json( [
+    {
+      id:1,
+      name:"john Doe",
+
+    },
+
+    {
+      id:2,
+      name:"John Doe",
+
+    },
+
+    {
+      id:3,
+      name:"John Doe",
+    }
+  ]);
 }
