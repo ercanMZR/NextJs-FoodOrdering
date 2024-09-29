b31f1743f9294f5a1e1c50b508c594a033606c92
Ercan MUZIR <ercanmuzir@gmail.com>
2024-09-29 16:57:55 +0300

    React-Toastify added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,14 +4,17 @@ import "../styles/globals.css";
 import Layout from "./layout/Layout";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
-
 import { SessionProvider } from "next-auth/react"
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
  function MyApp({ Component, pageProps:{session,...pageProps} }) {
   return (
    <SessionProvider session={session}>
     <Provider store={store}>
      <Layout>
+      <ToastContainer></ToastContainer>
       <Component {...pageProps} />
     </Layout>
    </Provider>
